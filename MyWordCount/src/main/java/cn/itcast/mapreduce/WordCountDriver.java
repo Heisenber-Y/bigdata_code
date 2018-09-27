34b3ebc9123e2c7ffe03dddb52165d8611a38d32
menglongye <menglongye1@gmail.com>
2018-09-28 01:13:48 +0800

    11

--- MyWordCount/src/main/java/cn/itcast/mapreduce/WordCountDriver.java
+++ MyWordCount/src/main/java/cn/itcast/mapreduce/WordCountDriver.java
@@ -24,7 +24,7 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
  * 		以及其他各种所需要的参数
  */
 public class WordCountDriver {
-
+	//哈哈
 	public static void main(String[] args) throws Exception {
 	Configuration conf=	new Configuration();
 		Configuration conf = new Configuration();
